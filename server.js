5bbd20ee781835d87cd068675057cfeeaeb3a9e9
Revati <revati.v.bordawekar@gmail.com>
2023-04-19 21:46:06 -0700

    updates

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // Serve static files from the build directory
-app.use(express.static(path.join(__dirname, 'client/.next')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Set up routes
 app.post('/api/websites', createWebsite);
