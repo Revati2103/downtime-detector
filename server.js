5c3b233852e6e5dd4085d170d21196ca7b097dbc
Revati <revati.v.bordawekar@gmail.com>
2023-04-20 21:51:33 -0700

    added access control headers

--- server.js
+++ server.js
@@ -14,12 +14,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200,
-}
 
-app.use(cors(corsOptions))
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, 'client/out')));
