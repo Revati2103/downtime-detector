5b8ca7d185af9c312889d9c61ca1d2241f0a0f27
Revati <revati.v.bordawekar@gmail.com>
2023-04-21 11:12:49 -0700

    added Access control headers to server side

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, 'client/out')));
