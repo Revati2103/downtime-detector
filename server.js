0306fbb5a8097c23fc04e78ba32a7fb298f97ebc
Revati <revati.v.bordawekar@gmail.com>
2023-04-21 13:36:35 -0700

    handling preflight

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cors())
 
+// handle preflight requests
+app.options('*', cors());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
