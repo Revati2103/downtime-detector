e4fae9debe33d3dac5d1dc93f84cfa6a18c2cd6b
Revati <revati.v.bordawekar@gmail.com>
2023-04-22 07:38:55 -0700

    fixed next/Link error

--- client/pages/snooze-info.js
+++ client/pages/snooze-info.js
@@ -1,15 +1,18 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function SnoozeInfo() {
   const router = useRouter();
   const { url } = router.query;
 
   return (
-    <div className="flex flex-col items-center mt-8">
-      <h3 className="text-lg font-medium mb-4">Success!</h3>
-      <p className="text-center mb-4">Alerts for {url} have been permanently snoozed.</p>
-      <p className="text-center mb-8">If you want to monitor your website again, please use the URL submission form.</p>
-      <a href="/" className="text-blue-500 hover:text-blue-700 underline">Back to home page</a>
+    <div className="max-w-xl mx-auto px-4">
+      <h3 className="text-2xl font-semibold mb-4">Success!</h3>
+      <p className="mb-4">
+        Alerts for {url} have been permanently snoozed. If you want to monitor your website again, please use the URL submission form.
+      </p>
+      <Link href="/">
+        <a className="text-blue-500 hover:text-blue-700">URL Submission Form</a>
+      </Link>
     </div>
   );
 }
