634cb8e0c1d0453b1a5bdf52748659fff276ca57
Revati <revati.v.bordawekar@gmail.com>
2023-04-25 13:22:16 -0700

    strictQuery set to true

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI)
     console.log(`MongoDB Connected ${conn.connection.host}`)
+    mongoose.set('strictQuery', true);
   } catch (error) {
     console.log(error)
     process.exit(1)
