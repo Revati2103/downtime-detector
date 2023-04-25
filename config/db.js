8c243df9fc90708bc08ee02ef08e5561fbcb67dc
Revati <revati.v.bordawekar@gmail.com>
2023-04-25 11:53:48 -0700

    updated mongo uri env

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
+//updated env var name for vercel
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI)
