57f12500c5233215bd97611b5abe1aeab4e4ea5b
Revati <revati.v.bordawekar@gmail.com>
2023-04-25 22:47:09 -0700

    fixed success url

--- controllers/snoozeController.js
+++ controllers/snoozeController.js
@@ -1,4 +1,6 @@
 const Website = require('../models/Website');
+const dotenv = require('dotenv');
+require('dotenv').config();
 
 const snoozeUrl = async (req, res) => {
   try {
@@ -14,7 +16,7 @@ const snoozeUrl = async (req, res) => {
     }
     //return res.status(200).json({ success: true });
     // Redirect user to /app/snooze-info/:id
-    return res.redirect(`/snooze-info`);
+    return res.redirect(`${process.env.URL_PREFIX}/snooze-info`);
   } catch (error) {
     console.log(`Error snoozing website: ${error.message}`);
     return res.status(500).json({success:false, message: 'Internal server error' });
