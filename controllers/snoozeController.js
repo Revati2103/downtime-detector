ac419618c179888b410de0844b152074342cca29
Revati <revati.v.bordawekar@gmail.com>
2023-04-26 10:12:22 -0700

    added success json

--- controllers/snoozeController.js
+++ controllers/snoozeController.js
@@ -14,9 +14,9 @@ const snoozeUrl = async (req, res) => {
     if (!website) {
       return res.status(404).json({ success: false, message: 'Website not found or already snoozed' });
     }
-    //return res.status(200).json({ success: true });
+    return res.status(200).json({ success: true });
     // Redirect user to /app/snooze-info/:id
-    return res.redirect(301, 'https://downtime-detector.vercel.app/snooze-info');
+    // return res.redirect(301, 'https://downtime-detector.vercel.app/snooze-info');
   } catch (error) {
     console.log(`Error snoozing website: ${error.message}`);
     return res.status(500).json({success:false, message: 'Internal server error' });
@@ -29,7 +29,8 @@ const getSnoozeInfo = async (req, res) => {
     if (!website) {
       return res.status(404).json({ message: 'Website not found' });
     }
-    return res.status(200).json({ website });
+    //return res.status(200).json({ website });
+    return res.status(200).redirect('https://downtime-detector.vercel.app/snooze-info');
   } catch (error) {
     console.log(`Error getting website info: ${error.message}`);
     return res.status(500).json({ message: 'Internal server error' });
