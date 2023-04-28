9114d1c22c270919a9df63ec2fb2d9c5aa5bed38
Revati <revati.v.bordawekar@gmail.com>
2023-04-28 13:16:54 -0700

    testing node-schedule

--- jobs/cron.js
+++ jobs/cron.js
@@ -1,8 +1,15 @@
-const cron = require('node-cron');
+//const cron = require('node-cron');
+const schedule = require('node-schedule');
 const { checkWebsites } = require('./websiteJob');
 
-const job = cron.schedule('* * * * *', () => {
-  checkWebsites();
-});
+// const job = cron.schedule('* * * * *', () => {
+//   checkWebsites();
+// });
+
+// Schedule the job to run every minute
+const job = schedule.scheduleJob('* * * * *', () => {
+    checkWebsites();
+  });
+  
 
 module.exports = job;
