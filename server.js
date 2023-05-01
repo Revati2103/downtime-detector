09dc84a8158bd1c0a21c5cea110c3d374aeea4c7
Revati <revati.v.bordawekar@gmail.com>
2023-05-01 10:03:17 -0700

    node-cron

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const cron = require('node-cron');
+const cron = require('node-cron');
 const cors = require('cors');
 const path = require('path');
 const port = process.env.PORT || 5500;
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 require('dotenv').config();
 //const job = require('./jobs/cron');
 //const CronJob = require('cron').CronJob;
-const schedule = require('node-schedule');
+
 
 
 const connectDB = require('./config/db');
@@ -60,11 +60,6 @@ app.use((err, req, res, next) => {
 });
 
 
-const job = schedule.scheduleJob('* * * * *', () => {
-  console.log('Inside node-schedule...')
-  checkWebsites();
-});
-
 // const job = new CronJob('* * * * * *', checkWebsites, null, true, 'America/Los_Angeles');
 // job.start();
 
@@ -76,9 +71,9 @@ const job = schedule.scheduleJob('* * * * *', () => {
 // });
 
 //node-cron
-// cron.schedule('* * * * *', () => {
-//   checkWebsites();
-// })
+cron.schedule('* * * * *', () => {
+  checkWebsites();
+})
 
 app.get('/', (req,res) => {
     res.send('Hello from 5500!');
