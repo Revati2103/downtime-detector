b9086a1a6215463f3e605f36a1a96c14f0639750
Revati <revati.v.bordawekar@gmail.com>
2023-04-30 22:28:09 -0700

    trying cron package

--- jobs/cron.js
+++ jobs/cron.js
@@ -1,11 +1,6 @@
-
-const schedule = require('node-schedule');
+const CronJob = require('cron').CronJob;
 const { checkWebsites } = require('./websiteJob');
 
-const job = schedule.scheduleJob('* * * * *', () => {
-    checkWebsites();
-  });
-
-  
+const job = new CronJob('* * * * * *', checkWebsites, null, true, 'America/Los_Angeles');
 
 module.exports = job;
